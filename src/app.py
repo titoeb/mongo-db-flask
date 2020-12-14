6926af2168d4938b39ca0f0785edbc1c2fa74c3d
titoeb <timtoebrock@gmail.com>
2020-12-14 07:45:43 +0100

    Added db connection

--- src/app.py
+++ src/app.py
@@ -1,10 +1,29 @@
 from flask import Flask
+from flask_restful import Api, Resource
+from pymongo MongoClient
 
+# Set-up App
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello_world():
-	return "Hello World from inside a docker container that is hosted in docker-compoes."
+# Initiallize db connection
+client = MongoClient("mongodb://mongo-db:27017")
+db = client.aNewDB
+# Select the UserNum collection
+UserNum = db["UserNum"]
+# Initiallize the number of user in the website as 0.
+UserNum.insert({
+    "number_of_users": 0
+})
+
+class Visit(Resource):
+    def get(self):
+        previous_number = UserNum.find({})[0]["number_of_users"]
+        current_number = previous_number + 1
+        UserNum.update({}, {$set:{"number_of_users": current_number}})
+        return f"Hello user {current_number}"
+
+api.add_resource(Visit, "/visit")
 
 if __name__ == "__main__":
     	app.run(debug=True)
