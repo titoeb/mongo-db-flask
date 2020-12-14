239e153c17adf94c3115ea0d1cf80fb7888b8e87
titoeb <timtoebrock@gmail.com>
2020-12-14 08:55:15 +0100

    Running version of application

--- src/app.py
+++ src/app.py
@@ -12,6 +12,19 @@ client = MongoClient("mongodb://mongo-db:27017")
 db = client.SentencesDatabase
 users = db["users"]
 
+# Helper functions
+def verify_user(username:str, password: str) -> bool:
+    hashed_pw = users.find({
+        "Username": username
+        })[0]["Password"]
+
+    return bcrypt.hashpw(password.encode("utf8"), hashed_pw) == hashed_pw
+
+def count_tokens(username:str) -> bool:
+    return users.find({
+        "Username": username
+        })[0]["Tokens"]
+
 class Register(Resource):
     def post(self):
         postedData = request.get_json()
@@ -25,7 +38,8 @@ class Register(Resource):
         users.insert({
             "Username": username,
             "Password": hashed_password,
-            "sentence": ""
+            "Tokens": 5,
+            "Sentence": ""
         })
         retJson = {
                 "status": 200,
@@ -33,7 +47,65 @@ class Register(Resource):
         }
         return jsonify(retJson)
 
+class Store(Resource):
+    def post(self):
+        postedData = request.get_json()
+        # Parse input data.
+        username = postedData["username"]
+        password = postedData["password"]
+        sentence = postedData["sentence"]
+
+        # Verify username password combination
+        correct_pw = verify_user(username, password)
+        if not correct_pw:
+            return jsonify({
+                "status": 302,
+                "msg": "Username and password don't match"})
+        # Verify user has enough tokens
+        number_tokens = count_tokens(username)
+        if number_tokens <= 0:
+            return(jsonify({
+                    "status": 301,
+                    "msg": "You don't have enough tokens!"
+                }))
+
+        # Store sentence and return sucess.
+        users.update({"Username": username}, {"$set": {"Sentence": sentence, "Tokens": number_tokens-1}})
+        return(jsonify({
+            "status": 200,
+            "msg": "Sentence was stored succesfully"
+             }))
+
+class Get(Resource):
+    def get(self):
+        postedData = request.get_json()
+        
+        username = postedData["username"]
+        password = postedData["password"]
+        
+        # Verify username password combination
+        correct_pw = verify_user(username, password)
+        if not correct_pw:
+            return jsonify({
+                "status": 302,
+                "msg": "Username and password don't match"})
+        # Verify user has enough tokens
+        number_tokens = count_tokens(username)
+        if number_tokens <= 0:
+            return(jsonify({
+                    "status": 301,
+                    "msg": "You don't have enough tokens!"
+                }))
+
+        sentence = users.find({
+            "Username": username})[0]["Sentence"]
+
+        return(jsonify({"status": 200, "msg": sentence}))
+
+
 api.add_resource(Register, "/register")
+api.add_resource(Store, "/store")
+api.add_resource(Get, "/get")
 
 if __name__ == "__main__":
     	app.run(debug=True)
